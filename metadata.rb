ca134566b82a06a3f28c703c25343841011cbc21
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-07-28 18:38:05 -0400

    Inital commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'buildbot'
+maintainer       'Eddie Hurtig'
+maintainer_email 'eddie@hurtigtechnologies.com'
+license          'apache2'
+description      'Installs/Configures buildbot'
+long_description 'Installs/Configures buildbot'
+version          '0.0.1'
+
+supports 'ubuntu', '>= 12.04'
