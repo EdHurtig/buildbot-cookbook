c0773e2dc2b76dae1834bc5bb90342578159dc6e
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-07-29 00:29:39 -0400

    Add more to the buildbot_slave provider

--- providers/slave.rb
+++ providers/slave.rb
@@ -1,12 +1,23 @@
 action :install do
+  directory new_resource.path do
+    owner new_resource.owner
+    group new_resource.group
+    recursive true
+  end
+
   python_virtualenv new_resource.path do
     owner new_resource.owner
     group new_resource.group
     action :create
   end
 
-  python_pip 'buildbot' do
+  python_pip 'buildbot-slave' do
     virtualenv new_resource.path
+  end
+
+  execute 'name' do
+    command ". bin/activate && buildslave create-slave #{new_resource.path} #{new_resource.master_host}:#{new_resource.master_port} #{new_resource.name} #{new_resource.password} #{new_resource.custom}"
+    cwd new_resource.path
   end
 end
 
--- resources/slave.rb
+++ resources/slave.rb
@@ -1,15 +1,14 @@
 actions :create, :remove
 
-attribute :name,           kind_of: String, name_attribute: true
-attribute :path,           kind_of: String
+attribute :name,        kind_of: String, name_attribute: true
+attribute :path,        kind_of: String
 
 
-attribute :owner,          kind_of: Array, default: 'root'
-attribute :group,          kind_of: Array, default: 'root'
-# attribute :baragon_port,   kind_of: Integer
-# attribute :baragon_group,  regex: /^([a-z]|[A-Z]|[0-9]|_|-)+$/
-# attribute :upstreams,      kind_of: Hash
-# attribute :proxies,        kind_of: Hash
-# attribute :include_static
+attribute :owner,       kind_of: String, default: 'buildslave'
+attribute :group,       kind_of: String, default: 'buildslave'
+attribute :master_host, kind_of: String, default: 'localhost'
+attribute :master_port, kind_of: Integer, default: 42012
+attribute :password,    kind_of: String
+attribute :custom,      kind_of: String
 
 default_action :install
